952e7119b2edc81c855f3443e49744594b6a5a87
SNmcdarby1 <sngereso@gmail.com>
2021-07-08 16:40:56 +0000

    added checkout static,views,urls

--- conxia/settings.py
+++ conxia/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+      # Other
+    'crispy_forms',
     
 ] 
 
@@ -67,6 +70,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'conxia.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -81,8 +86,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', #allauth requirement
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },    
 ]
